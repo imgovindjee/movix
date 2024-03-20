adc5b89a1fb4fd25bcbd368922dd4e2938c54ebf
govindjeeje@gmail.com <govindjeeje@gmail.com>
2024-03-20 18:22:28 +0530

    Error Page Fixing Done

--- src/Pages/error(404)/Error.jsx
+++ src/Pages/error(404)/Error.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import ContentWrapper from '../../Components/contentWrapper/ContentWrapper'
+
 import "./Error.scss"
 
 const Error = () => {
     return (
-        <div>
-
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
         </div>
     )
 }
